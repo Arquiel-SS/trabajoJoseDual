cfe15e2c9aac40479826f7f86c477e1c27be968b
Arquiel-SS <gabrielsantandreusantonja@gmail.com>
2025-04-30 19:13:15 +0200

    Añadido endpoint para añadir usuarios

--- app.py
+++ app.py
@@ -148,6 +148,24 @@ class Concierto(db.Model):
 def mostrar_opciones_index():
     return render_template('index.html')
 
+@app.route('/add_integrante', methods=["GET", "POST"])
+def agregar_user():
+    if request.method == "POST":
+        nombre = request.form['id']
+        apellidos = request.form['apellidos']
+        edad = request.form['edad']
+        nacionalidad = request.form['nacionalidad']
+        fecha_nacimiento = request.form['fecha_nacimiento']
+        rol = request.form['rol']
+
+        return f"Gracias por añadir un integrante!"
+    
+    new_integrante = Integrante(nombre, apellidos, edad, nacionalidad, fecha_nacimiento, rol)
+    db.session.add(new_integrante)
+    db.session.commit()
+    
+    return render_template('integrantes.html')
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
